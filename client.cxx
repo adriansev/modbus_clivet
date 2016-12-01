1afc9933c26021f8d1f6b218049fe912f3feb70f
asevcenc <adrian.sevcenco@cern.ch>
2016-12-01 21:14:00 +0200

    split temp readings from electrical readings ... addresses still wrong and not working

--- client.cxx
+++ client.cxx
@@ -15,6 +15,7 @@
 #include "modbus/modbus.h"
 
 #define MIXED_DATA_ADDR 500
+#define ELECTRIC_ADDR 532
 #define SOFTWARE_VER_ADDR 1
 
 // REFERENCE DOCUMENT
@@ -23,8 +24,11 @@
 int main () {
 modbus_t *mb = NULL;
 
-uint16_t mixed_data_tbl [34];
+uint16_t mixed_data_tbl [18];
 std::memset(mixed_data_tbl, 0, sizeof mixed_data_tbl); // set the content to 0
+
+uint16_t electric_tbl [3];
+std::memset(electric_tbl, 0, sizeof electric_tbl); // set the content to 0
 
 uint16_t software_ver_tbl [4];
 std::memset(software_ver_tbl, 0, sizeof software_ver_tbl); // set the content to 0
@@ -38,6 +42,7 @@ if (modbus_connect(mb) == -1) {
     return -1;
     }
 
+//#######################################################################################################################
 // Read the software version table
 // info at page 5, begining of the page
 err_val = modbus_read_registers(mb, SOFTWARE_VER_ADDR, sizeof(software_ver_tbl)/sizeof(software_ver_tbl[0]), software_ver_tbl);
@@ -55,6 +60,7 @@ std::cout << "Firmware screen = " << firm_screen << std::endl;
 std::cout << "Firmware revision = " << firm_rev  << std::endl;
 std::cout << "Firmware revision E2 = " << firm_rev_e2  << std::endl;
 
+//#######################################################################################################################
 // Read the full size of table starting with MIXED_DATA_ADDR
 // info at page 12, end of the page table
 err_val = modbus_read_registers(mb, MIXED_DATA_ADDR, (int)(sizeof(mixed_data_tbl)/sizeof(mixed_data_tbl[0])), mixed_data_tbl);
@@ -75,11 +81,25 @@ for (int i=0; i < (int)(sizeof(mixed_data_tbl)/sizeof(mixed_data_tbl[0])); i++) 
     printf("mixed data table :: reg[%d]=%d (0x%X)\n", i, mixed_data_tbl[i], mixed_data_tbl[i]);
     }
 
+//#######################################################################################################################
+// Read the full size of table starting with MIXED_DATA_ADDR
+// info at page 12, end of the page table
+err_val = modbus_read_registers (mb, ELECTRIC_ADDR, (int)(sizeof(electric_tbl)/sizeof(electric_tbl[0])), electric_tbl);
+if (err_val == -1) {
+    std::cerr << "Reading registers : " << modbus_strerror(errno) << std::endl;
+    modbus_free(mb);
+    return -1;
+    }
+
+for (int i=0; i < (int)(sizeof(electric_tbl)/sizeof(electric_tbl[0])); i++) {
+    printf("electric data table :: reg[%d]=%d (0x%X)\n", i, electric_tbl[i], electric_tbl[i]);
+    }
+
+
 
 modbus_close(mb);
 modbus_free(mb);
 return 0;
 }
-
 
 
